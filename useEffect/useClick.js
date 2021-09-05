49248e26c823623cd8960bb6664f6be5fcb168aa
OZOscarZulu <ktstyle83@gmail.com>
2021-09-05 23:49:20 +0900

    add:useClick

---
+++ useEffect/useClick.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const useClick = (onClick) => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
+const App = () => {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+
+export default App;
+
+//reference는 component의 어떤 부분을 선택
+//document.getElementById()를 사용한것과 동등
